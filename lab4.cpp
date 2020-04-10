fcfbb4ecf0d43fcead6f176f4161b0fab70b6be7
Miroslaw Puczynski <miroslaw@puczynski.org>
2020-04-10 15:38:18 +0200

    add 2nd function

--- lab4.cpp
+++ lab4.cpp
@@ -24,9 +24,12 @@ vector<int> uporzadkuj(const vector<int> & dane);
 
 vector<double> t1 = {2.0, 5.0, 8.0, 16.0, 25.0};
 vector<double> t2 = {6.0, 7.0, 14.0, 22.0};
-vector<double> wynik;
+const vector<int> t3 = {2,1,8,6,7,9,5,3,4};
+vector<double> wynik1;
+vector<int> wynik2;
 
-void wyswietl(vector<double> t);
+string wyswietl(vector<double> t);
+string wyswietl(vector<int> t);
 
 
 int main(int argc, char const *argv[])
@@ -39,21 +42,26 @@ int main(int argc, char const *argv[])
 void menu(){
   int wybor = 0;
   do {
+    cout <<  endl;
     cout << "MENU: " << endl;
     cout << "(1) funkcja SUMA" << endl;
     cout << "(2) funkcja UPORZADKUJ" << endl;
     cout << "(3) Sprawdz biale znaki" << endl;
     cout << "(0) Wyjdz" << endl;
+    cout <<  endl;
     cin >> wybor;
     int rok;
     switch (wybor)
     {
     case 1:
-        wynik = suma(t1,t2);
-        wyswietl(wynik);
+        wynik1 = suma(t1,t2);
+        cout << wyswietl(t1) << ", " << wyswietl(t2) << " daje w wyniku: " << endl;
+        cout << wyswietl(wynik1);
       break;
     case 2:
-
+        wynik2 = uporzadkuj(t3);
+        cout << "Po uporzadkowaniu wektor: " << wyswietl(t3) << " daje w wyniku: " << endl;
+        cout << wyswietl(wynik2);
       break;
     case 3:
 
@@ -71,16 +79,52 @@ vector<double> suma(vector<double> t1, vector<double> t2){
   for(int i = 0; i < minSize; i++){
     out.push_back(t1[i] + t2[i]);
   }
+  return out;
 }
 
-void wyswietl(vector<double> t){
-    cout << "[";
+vector<int> uporzadkuj(const vector<int> & dane){
+  vector<int> out;
+  vector<int> nieparzyste;
+  for(int i = 0; i < dane.size(); i++){
+    if (dane[i] % 2 == 0){
+      out.push_back(dane[i]);
+    } else {
+      nieparzyste.push_back(dane[i]);
+    }
+  }
+
+  for(int i = 0; i < nieparzyste.size(); i++){
+    out.push_back(nieparzyste[i]);
+  }
+  return out;
+}
+
+string wyswietl(vector<double> t){
+  char buffer[100];
+    string out =  "[";
     for(int i = 0; i < t.size(); i++){
       if (i > 0){
-        cout << ", ";
+        out += ", ";
       }
-      cout << t[i];
+      sprintf(buffer, "%.2f", t[i]);
+      out += buffer;
     }
-    cout << "]";
+    out += "]";
+    return out;
+}
+
+string wyswietl(vector<int> t){
+  char buffer[100];
+
+  string out =  "[";
+  for(int i = 0; i < t.size(); i++){
+    if (i > 0){
+      out += ", ";
+    }
+    sprintf(buffer, "%d", t[i]);
+    out += buffer;
+  }
+  out += "]";
+  return out;
 }
 
